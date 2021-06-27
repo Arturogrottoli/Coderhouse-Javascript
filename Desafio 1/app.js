6a25b225d1e46fc3e6d60183e7b68efc74bd9123
Arturo <77126022+Arturogrottoli@users.noreply.github.com>
2021-06-27 20:32:47 -0300

    Add files via upload

---
+++ Desafio 1/app.js
@@ -0,0 +1,26 @@
+console.log("Desafio 1")
+
+let nombre = prompt("Ingrese su nombre");
+let apellido = prompt("Ingrese su apellido");
+
+let saludo = "Bienvenido " + nombre + " " + apellido + " vamos a hacer unos calculos!" ;
+
+alert(saludo);
+
+
+let numero1 = parseInt(prompt("Ingrese el primer numero"));
+
+let numero2 = parseInt(prompt("Ingrese el segundo numero"));
+
+alert("Para ver la suma y la resta de los numeros ingrese a la consola");
+
+let suma = "El resultado de la suma es " + (numero1 + numero2);
+console.log(suma);
+
+let resta = "El resultado de la resta es " + (numero1 - numero2);
+console.log(resta);
+
+
+
+
+
