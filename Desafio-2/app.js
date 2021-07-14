96bc7780c170ee59bdbba94a8dd5d55afb382abe
Arturo <77126022+Arturogrottoli@users.noreply.github.com>
2021-07-14 13:00:50 -0300

    Add files via upload

---
+++ Desafio-2/app.js
@@ -0,0 +1,25 @@
+console.log("Desafio 2")
+
+// Pregunta nombre y edad, deja ingresar solo a los mayores de edad
+
+let nombre = prompt("Ingrese su nombre");
+
+let edad = Number(prompt("Ingrese su edad"));
+
+
+if(edad >= 18){
+
+alert(`Hola ${nombre}, bienvenido al sitio web!`);
+
+}else {
+
+    alert(`Eres menor de edad ${nombre}, no puedes entrar a esta web`);
+    
+} 
+
+
+
+
+
+
+
