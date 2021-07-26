89d6a781e46d77964bcf723d467d00fee59e5222
Arturo <77126022+Arturogrottoli@users.noreply.github.com>
2021-07-26 14:04:09 -0300

    Add files via upload

---
+++ funcion-scroll-top-jquery/app2.js
@@ -0,0 +1,28 @@
+scrollTopButton('.scroll-top-btn');
+
+function scrollTopButton(btn){
+    const $ScrollBtn = $(btn);
+    
+    $(window).scroll(function(){
+        let scrollTop = $(this).scrollTop();
+        console.log(scrollTop);
+        //Operador ternario sustituyendo al if{} else{} 
+        scrollTop > 400 ? $ScrollBtn.removeClass('hidden') : $ScrollBtn.addClass('hidden');
+    })
+
+    $ScrollBtn.click(function(){
+        window.scrollTo({
+            behavior: 'smooth',
+            top: 0
+        })
+    })
+}
+
+//Pasar sin acento
+
+var originalText = "Ciencia Ficción"
+var result = originalText.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
+console.log(result)
+
+
+
